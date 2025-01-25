897be68549fc552576b5fa859e8032a77c32815f
Rafael Cortes <rafael_cortes@outlook.com>
2025-01-25 18:04:39 -0300

    HelloWorld

---
+++ examples/chapter01_02/01-HelloWorld.js
@@ -0,0 +1,12 @@
+// @ts-check
+/* eslint-disable */
+
+// ******* Básico sobre o JavaScript ********
+
+function output(t) {
+    document.write('<p>' + t + '</p>');
+}
+
+alert('Hello, World!');
+console.log('Hello, World!');
+output('Hello, World!');
